66365259da5b0aedb341206fb03cebf96d2aa0bb
young-nan <youngwisdom0250@gmail.com>
2026-06-16 08:19:12 +0100

    Delete src directory

--- src/components/brand/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * src/components/brand/index.js
- *
- * Centralised exports so callers write a single import:
- *   import { Mark, Logo, Wordmark } from '@/components/brand';
- */
-
-export { Mark } from './Mark';
-export { Wordmark, Logo } from './Wordmark';
